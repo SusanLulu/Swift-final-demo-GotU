8fd6c0cc50b60ac96d3f49c29479c5c058660499
Ping-Ying Yen <sherry40735@gmail.com>
2016-12-28 15:59:46 +0800

    Initial Commit

---
+++ GotUProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GotUProject
+//
+//  Created by Ping-Ying Yen on 2016/12/28.
+//  Copyright © 2016年 Ping-Ying Yen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
